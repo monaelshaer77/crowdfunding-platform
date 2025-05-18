ce1eed0c7b35eefcbea34c71d0865ada11d82c38
ahmed khaled <ahmedelads789@gmail.com>
2025-05-18 10:13:28 +0300

    update2

--- main1.js
+++ main1.js
@@ -143,3 +143,15 @@ document.addEventListener('DOMContentLoaded', () => {
   new FAQApp();
   new ContactForm('contactForm', 'successMessage');
 });
+
+function localContent(elementId,fileName)
+{
+    fetch(fileName)
+    .then(response=>response.text())
+    .then(data=>{
+        document.getElementById(elementId).innerHTML=data;
+    })
+    .catch(err=>console.log('Error loading content'));
+}
+localContent("header-container",'../components/header.html');
+localContent("footer-conteriner","../components/footer.html");
